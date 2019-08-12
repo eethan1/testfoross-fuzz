6d48ebcdca31165301fcbdde6126cf9ecd0922de
eethan1 <call132call132@gmail.com>
2019-08-12 05:40:44 -0400

    afl practice

---
+++ afl_practice/afl_1.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+  
+int main() {
+  char c[128];
+  scanf("%s",c);
+  if(c[0]=='d') if(c[1]=='e')  if(c[2]=='a')  if(c[3]=='d')  if(c[4]=='b')  if(c[5]=='e')  if(c[6]=='e')  if(c[7]=='f') abort();
+  printf("%s",c); 
+}
+                        
