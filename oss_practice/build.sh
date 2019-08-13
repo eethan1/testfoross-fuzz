843463b655a302b5095c4e2e132ca3080388181e
root <root@localhost.localdomain>
2019-08-13 01:53:22 -0400

    seperate to afl,libfuzzer,oss part

---
+++ oss_practice/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
+
+# build fuzzers
+# e.g.
+$CXX $CXXFLAGS  -std=c++11 -Iinclude \
+	$SRC/first_fuzzer.cc -o $OUT/0-first_fuzzer \
+     $LIB_FUZZING_ENGINE 
+
+clang++ -g -std=c++11 -stdlib=libc++ -fsanitize-address-use-after-scope -fsanitize=address -fsanitize-coverage=trace-pc-guard -Iinclude $SRC/first_fuzzer.cc -o $OUT/first_fuzzer $LIB_FUZZING_ENGINE
+
+clang++ -g -std=c++11 -stdlib=libc++ -fsanitize-address-use-after-scope -fsanitize=address,fuzzer -Iinclude $SRC/second_fuzzer.cc -o $OUT/second_fuzzer $LIB_FUZZING_ENGINE
+
+clang++ -g -std=c++11 -stdlib=libc++ -fsanitize-address-use-after-scope -fsanitize=fuzzer -fsanitize-coverage=trace-pc-guard -Iinclude $SRC/third_fuzzer.cc -o $OUT/third_fuzzer $LIB_FUZZING_ENGINE
